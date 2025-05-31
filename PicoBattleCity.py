d02f0bc7bb4f82f057ff7fe9ddece1cf53487234
CarterSnich <jmestreraclosa@gmail.com>
2025-05-31 13:21:55 +0800

    excluded Dpad conditionals to Button A

--- PicoBattleCity.py
+++ PicoBattleCity.py
@@ -1,51 +1,49 @@
 import framebuf
 from time import ticks_diff, ticks_ms
+from random import randint
 
 from PicoGame import PicoGame
 from BattleCity.PlayerTank import PlayerTank
-# from BattleCity.EnemyTank import EnemyTank
+from BattleCity.EnemyTank import EnemyTank
 from BattleCity.Tank import Direction
 
 
 def battle_city():
     game = PicoGame()
+    last_button_debounce = -1
     
     x, y = game.get_center(4, 4)
     player = PlayerTank(x, y)
     
-    last_button_debounce = -1
+    last_enemy_deploy_ms = ticks_ms()
+    enemy_tanks = []
+    
     
     while True:
-        
+        # exit game loop
         if game.button_B():
             break
         
+        # update states
         player.update()
         
+        # render
         game.fill(0)
-        game.blit(player.get_sprite(), player.x, player.y)
-        
-        for pb in player.bullets:
-            game.pixel(pb[0], pb[1], 1)
-        
+        player.render(game)
         game.show()
         
+        # inputs
         debounce_delta = ticks_diff(ticks_ms(), last_button_debounce) >= 300
-        
         if game.button_A() and debounce_delta:
             last_button_debounce = ticks_ms()
-            player.fire()  
-        elif game.button_up():
-            last_button_debounce = ticks_ms()
+            player.fire()    
+        if game.button_up():
             player.up()
         elif game.button_right():
-            last_button_debounce = ticks_ms()
             player.right()
         elif game.button_down():
-            last_button_debounce = ticks_ms()
             player.down()
         elif game.button_left():
-            last_button_debounce = ticks_ms()
             player.left()
                 
 
