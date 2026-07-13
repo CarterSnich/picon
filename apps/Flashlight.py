158f0b467732d4f51642ceb2c609d9b786ada471
CarterSnich <jmeclosa@proton.me>
2026-07-13 17:52:23 +0800

    fix KEY_A not work in strobe mode

--- apps/Flashlight.py
+++ apps/Flashlight.py
@@ -17,20 +17,23 @@ class Main(PiconApp):
     last_strobe_ms = 0
     strobe_delay = 300
 
+
     def __init__(self, display, input, sound):
         super().__init__(display, input, sound)
+
 
     def inputs(self):
         if self.input.is_pressed(KEY_B):
             self.led_state = LED_OFF
             self.quit()
+        elif self.input.is_pressed(KEY_A):
+            self.led_state = (self.led_state + 1) % 3
         elif self.led_state == LED_STROBE:
             if self.input.is_pressed(DPAD_UP) and self.strobe_delay < 3000:
                 self.strobe_delay += 50
             elif self.input.is_pressed(DPAD_DOWN) and self.strobe_delay > 50:
                 self.strobe_delay -= 50
-        elif self.input.is_pressed(KEY_A):
-            self.led_state = (self.led_state + 1) % 3
+
 
     def update(self):
         if self.led_state == LED_OFF:
@@ -42,6 +45,7 @@ class Main(PiconApp):
                 self.last_strobe_ms = ticks_ms()
                 self.led.value(not self.led.value())
 
+
     def render(self):
         if self.led_state == LED_STROBE:
             self.print("STROBE DELAY", str(self.strobe_delay))
@@ -49,9 +53,15 @@ class Main(PiconApp):
             self.led.value(self.led_state)
             self.print("FLASHLIGHT", "ON" if self.led_state else "OFF")
 
+
     def print(self, str1, str2):
         x = lambda s: int(SCREEN_WIDTH / 2) - int((len(s) / 2) * 8)
         y = int(SCREEN_HEIGHT / 2)
         self.display.text(str1, x(str1), y - 8)
         self.display.text(str2, x(str2), y + 8)
 
+
+if __name__ == '__main__':
+    from core import Display, Input, Sound
+
+    Main(Display(), Input(), Sound()).run()
