130af1fdb28edd3a325a961c97066fc794e6eb37
CarterSnich <jmeclosa@proton.me>
2026-07-26 14:20:15 +0800

    Racing Game sounds and road line animations

--- apps/RacingGame/__init__.py
+++ apps/RacingGame/__init__.py
@@ -1,31 +1,40 @@
 from random import randrange
-from time import ticks_diff
 
 from core.app import PiconGame
 from core.config import SCREEN_HEIGHT, SCREEN_WIDTH
 from core.input import DPAD_UP, DPAD_DOWN, KEY_A
+from core.helper import has_elapsed, has_not_elapsed
 
 from apps.RacingGame.racer import Racer
 from apps.RacingGame.civilian import Civilian
 
-ROAD_LINES_Y = (10, 52, 94)
 STARTING_INTERVAL = 50
 MAX_CIVILLIANS = 3
+
+ROAD_LINE_WIDTH = 20
+ROAD_LINE_COUNT = 4  # 3 visible + 1 off-screen
+ROAD_LINE_SPACING = 43  # round(128 / 3)
+ROAD_LINE_CYCLE = ROAD_LINE_SPACING * ROAD_LINE_COUNT
+ROAD_LINE_X = [11, 53, 96, 139]
+
+NORMAL_SPEED_TONE = 200
+NOS_SPEED_TONE = 220
 
 
 class Main(PiconGame):
 
     def __init__(self, display, input, sound):
         super().__init__(display, input, sound)
+        self.input.set_debounce(100)
 
         self.racer = Racer()
-        self.civillians = (
-            Civilian(128, randrange(3)),
-            Civilian(182, randrange(3)),
-            Civilian(236, randrange(3))
-        )
+        self.civillians = (Civilian(128, randrange(3)),
+                           Civilian(182, randrange(3)),
+                           Civilian(236, randrange(3)))
         self.interval = STARTING_INTERVAL
         self.nos_on = False
+
+        self.road_lines = ROAD_LINE_X
 
         self.last_update_ms = self.current_ms
 
@@ -42,9 +51,12 @@ class Main(PiconGame):
 
 
     def update(self):
-        if ticks_diff(self.current_ms, self.last_update_ms) < self.interval and self.nos_on is False:
+        self.sound.tone(NOS_SPEED_TONE if self.nos_on else NORMAL_SPEED_TONE)
+
+        if has_not_elapsed(self.current_ms, self.last_update_ms, self.interval) and self.nos_on is False:
             return
 
+        # Civillians
         for c in self.civillians:
             c.move()
             if c.is_colliding(self.racer):
@@ -57,6 +69,13 @@ class Main(PiconGame):
                 if self.interval:
                     self.interval -= 1
 
+        # Road lines
+        for i in range(len(self.road_lines)):
+            if self.road_lines[i] <= -ROAD_LINE_WIDTH:
+                self.road_lines[i] += ROAD_LINE_CYCLE
+            else:
+                self.road_lines[i] -= 1
+
         self.last_update_ms = self.current_ms
 
 
@@ -66,9 +85,9 @@ class Main(PiconGame):
         self.display.fill_rect(0, SCREEN_HEIGHT - 4, SCREEN_WIDTH, 4, 1)
 
         # Road lines
-        for rl_x in ROAD_LINES_Y:
-            self.display.fill_rect(rl_x, 20, 21, 4, 1)
-            self.display.fill_rect(rl_x, 40, 21, 4, 1)
+        for rl_x in self.road_lines:
+            self.display.fill_rect(rl_x, 20, 22, 4, 1)
+            self.display.fill_rect(rl_x, 40, 22, 4, 1)
 
         # Racer
         self.racer.draw(self.display, self.racer.x, self.racer.y)
